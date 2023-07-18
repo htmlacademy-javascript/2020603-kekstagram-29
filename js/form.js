e7d25dc9bb875ed65d272fe0c32c261df927c122
Omak <webmaster2511@yandex.ru>
2023-07-18 21:04:11 +0700

    ДЗ 9.13

--- js/form.js
+++ js/form.js
@@ -1,14 +1,48 @@
 import { isEscapeKey } from './util.js';
 
+const HASHTAGS_COUNT_MAX = 5;
+const SYMBOLS_VALID = /^#[a-za-яё0-9]{1,19}$/i;
+const ErrorMessages = {
+  INVALID_COUNT: `Максимум ${HASHTAGS_COUNT_MAX} хэштегов`,
+  INVALID_PATTERN: 'Неверно составлен хэштег(и)',
+  NOT_UNIQUE: 'Хэштеги должны быть уникальными'
+};
+
 const bodyElement = document.querySelector('body');
 const formElement = document.querySelector('.img-upload__form');
 const overlayElement = formElement.querySelector('.img-upload__overlay'); // модалка с выбором фильтра, полями хэштега и комментария и кнопки "Опубликовать". Изначально скрыт
 const fileUploadElement = formElement.querySelector('#upload-file'); // поле выбора файла для отправки (.img-upload__input ). Изначально скрыт
-const btnCloseElement = formElement.querySelector('#upload-cancel');
+const buttonCloseElement = formElement.querySelector('#upload-cancel');
 const hashtagsFieldElement = formElement.querySelector('.text__hashtags');
 const commentFieldElement = formElement.querySelector('.text__description');
+// const submitButtonElement = formElement.querySelector('.img-upload__submit'); // кнопка "Опубликовать" в модалке
 
 const isTextFieldFocused = () => document.activeElement === hashtagsFieldElement || document.activeElement === commentFieldElement; // есть ли фокус на текстовом поле
+
+// функции валидации BEGIN
+const pristine = new Pristine(formElement, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper'
+});
+
+const normalizeTags = (tagsString) => tagsString.trim().split(' ').filter((item) => Boolean(item.length));
+const hasValidCount = (tags) => normalizeTags(tags).length <= HASHTAGS_COUNT_MAX;
+const hasValidTags = (tags) => normalizeTags(tags).every((tag) => SYMBOLS_VALID.test(tag));
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = normalizeTags(tags).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+pristine.addValidator(hashtagsFieldElement, hasValidCount, ErrorMessages.INVALID_COUNT, 3, true);
+pristine.addValidator(hashtagsFieldElement, hasValidTags, ErrorMessages.INVALID_PATTERN, 2, true);
+pristine.addValidator(hashtagsFieldElement, hasUniqueTags, ErrorMessages.NOT_UNIQUE, 1, true);
+
+const onFormSubmit = (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+};
+// функции валидации END
 
 const showModal = () => {
   overlayElement.classList.remove('hidden');
@@ -17,14 +51,14 @@ const showModal = () => {
 
 const hideModalForm = () => {
   formElement.reset();
-  //pristine.reset();
+  pristine.reset();
   overlayElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
-  btnCloseElement.removeEventListener('click', hideModalForm);
+  buttonCloseElement.removeEventListener('click', hideModalForm);
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
-const onClickCloseBtn = () => {
+const onClickCloseButton = () => {
   hideModalForm();
 };
 
@@ -37,7 +71,7 @@ function onDocumentKeydown(evt) {
 
 const onfileUploadChange = () => {
   showModal();
-  btnCloseElement.addEventListener('click', onClickCloseBtn);
+  buttonCloseElement.addEventListener('click', onClickCloseButton);
   document.addEventListener('keydown', onDocumentKeydown);
 };
 
@@ -47,5 +81,7 @@ const renderModalForm = () => {
   fileUploadElement.addEventListener('change', onfileUploadChange);
   //addValidatorPristine();
 };
+
+formElement.addEventListener('submit', onFormSubmit);
 
 export { renderModalForm };
