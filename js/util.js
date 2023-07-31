451eb0610039f8060e2216433b7fab96c29d2bf5
Omak <webmaster2511@yandex.ru>
2023-07-31 11:01:52 +0700

    удалена функция throttle в util.js

--- js/util.js
+++ js/util.js
@@ -58,17 +58,4 @@ const debounce = (callback, timeoutDelay = TIMEOUT_DELAY) => {
   };
 };
 
-const throttle = (callback, delayBetweenFrames) => {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-};
-
 export { getRandomInteger, getRandomArrayElement, createIdGenerator, isEscapeKey, showAlert, debounce };
